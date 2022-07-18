03244f25015c99112accce6186ab8a6f1a29f29f
Mohammed-Elshaer <maelshaer83@gmail.com>
2022-07-18 10:13:14 +0200

    Updates

---
+++ precache-manifest.6955ec7bc0ab86da6ec1fab2f098015b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "71b07f70bc05b60cd397",
+    "url": "/Food-Web-Site/static/css/main.e14f3486.chunk.css"
+  },
+  {
+    "revision": "71b07f70bc05b60cd397",
+    "url": "/Food-Web-Site/static/js/main.71b07f70.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/Food-Web-Site/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "1c92e29ba944388c8a90",
+    "url": "/Food-Web-Site/static/css/2.82a7eea6.chunk.css"
+  },
+  {
+    "revision": "1c92e29ba944388c8a90",
+    "url": "/Food-Web-Site/static/js/2.1c92e29b.chunk.js"
+  },
+  {
+    "revision": "189fabc0e46b42056ade",
+    "url": "/Food-Web-Site/static/js/runtime~main.189fabc0.js"
+  },
+  {
+    "revision": "8240b31abe14036a1674e28b68a62f1c",
+    "url": "/Food-Web-Site/static/media/1.8240b31a.png"
+  },
+  {
+    "revision": "94bb73f0855b1f79d06fb527ea033c71",
+    "url": "/Food-Web-Site/static/media/1.94bb73f0.jpg"
+  },
+  {
+    "revision": "4e42494613296172bbc0eb257788dafe",
+    "url": "/Food-Web-Site/static/media/2 (1).4e424946.jpg"
+  },
+  {
+    "revision": "d21261dfc72a6e2d07a7a4f6d39139e0",
+    "url": "/Food-Web-Site/static/media/2.d21261df.png"
+  },
+  {
+    "revision": "0c8648e388de723245f651472cb10fad",
+    "url": "/Food-Web-Site/static/media/01.0c8648e3.jpg"
+  },
+  {
+    "revision": "9904ffc76896b67076cd431a01f2ac62",
+    "url": "/Food-Web-Site/static/media/02.9904ffc7.jpg"
+  },
+  {
+    "revision": "57842d1007a649cc10df6f9f7cd07e53",
+    "url": "/Food-Web-Site/static/media/03.57842d10.jpg"
+  },
+  {
+    "revision": "e0c0f2cc1d3515ebf715c229f9e1752d",
+    "url": "/Food-Web-Site/static/media/3.e0c0f2cc.jpg"
+  },
+  {
+    "revision": "d6d2abbf37c8cb5960315578ff8513f7",
+    "url": "/Food-Web-Site/static/media/33.d6d2abbf.jpg"
+  },
+  {
+    "revision": "d730f1830c6c1313f7e4862160b461c4",
+    "url": "/Food-Web-Site/index.html"
+  }
+];
